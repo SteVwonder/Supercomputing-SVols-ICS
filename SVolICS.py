7a2f3cc23704debd820f1a64ed94cfa648d85a4f
Stephen Herbein <stephen272@gmail.com>
2015-11-15 10:58:27 -0600

    Fixed issue when SciNet users tried to use the script

--- SVolICS.py
+++ SVolICS.py
@@ -31,12 +31,17 @@ from pytz import timezone
 # ICS writing
 import ics
 
+import string
+
 central_tz = timezone('US/Central')
 bad_time_re = re.compile(r'^([0-9]+)(AM|PM)')
 
 #################################################################
 ###    Function Declarations
 #################################################################
+
+def sanitize_string(in_str):
+    return filter(lambda x: x in string.printable, in_str)
 
 def fix_bad_times(time_str):
     '''
@@ -78,6 +83,37 @@ def convert_vol_time(vol_time, date):
 
     return begin_time, end_time
 
+def convert_attend_time(attend_time):
+    '''
+    convert_attend_time()
+    Input: A time
+    Return: A beginning time and ending time
+    How it works: Takes an attend time in from an event
+     dictionary. This function will correct improperly formatted times
+     (ex. 4pm instead of 4:00PM), and it will return a starting and
+     ending datetime object to use in the .ics output.
+    '''
+
+    # Split the date and times out by --
+    date, times = attend_time.split("--")
+
+    # Grab beginning and ending times
+    begin_str, end_str = times.split('-')
+
+    # Convert beginning time from a string into a datetime object
+    begin_str = "{}T{}".format(date, begin_str).upper()
+    begin_datetime = datetime.strptime(begin_str, "%Y-%m-%dT%H:%M")
+    begin_datetime = central_tz.localize(begin_datetime)
+    begin_time = begin_datetime.isoformat()
+
+    # Convert ending time from a string into a datetime object
+    end_str = "{}T{}".format(date, end_str).upper()
+    end_datetime = datetime.strptime(end_str, "%Y-%m-%dT%H:%M")
+    end_datetime = central_tz.localize(end_datetime)
+    end_time = end_datetime.isoformat()
+
+    return begin_time, end_time
+
 def save_calendar(filename, calendar):
     '''
      save_calendar()
@@ -99,14 +135,25 @@ def create_calendar(schedule):
     for event in schedule:
         date = event['Day']
         description = "{} - {}".format(event['Type'], event['Tag'])
-        for volunteer_time in event['Volunteer'].split(','):
-            begin_time, end_time = convert_vol_time(volunteer_time, date)
-            ics_event = ics.Event(name=event['Event'],
+        if 'Volunteer' in event:
+            for volunteer_time in event['Volunteer'].split(','):
+                begin_time, end_time = convert_vol_time(volunteer_time, date)
+                ics_event = ics.Event(name=event['Event'],
+                                      begin=begin_time,
+                                      end=end_time,
+                                      description=description,
+                                      location=event['Location'])
+                calendar.events.append(ics_event)
+        elif 'Attend' in event:
+            begin_time, end_time = convert_attend_time (event['Time'])
+            ics_event = ics.Event(name=sanitize_string(event['Event']),
                                   begin=begin_time,
                                   end=end_time,
                                   description=description,
                                   location=event['Location'])
             calendar.events.append(ics_event)
+        else:
+            print "Warning: Event found that you are neither attending nor volunteering for: {}".format(event['Event'])
     return calendar
 
 def read_schedule(filename):
